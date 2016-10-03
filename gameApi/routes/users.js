d1c4def77d5a44fa6497dde8fece49580f0c6ecc
yGuez <yvesguezennec@gmail.com>
2016-10-03 15:12:40 +0200

    add front

--- gameApi/routes/users.js
+++ gameApi/routes/users.js
@@ -1,9 +1,20 @@
 var express = require('express');
+var nano = require('nano')('http://localhost:5984').use("user_y"); 
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/users/', function(req, res, next) {
   res.send('respond with a resource');
+  user_y.get(id, function (error, existing) {
+        if (!error) {
+            mylib.destroy(id, existing._rev, function (err, body, header) {
+                if (!error) {
+                    response.writeHead(200, { "Content-Type": "text/plain" });
+                    response.end("Book deleted. Response: " + JSON.stringify(body) + "\n");
+                }
+            })
+        }
+    });
 });
 
 module.exports = router;
---
+++ gameFront/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gameFrontApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the gameFrontApp
+ */
+angular.module('gameFrontApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
